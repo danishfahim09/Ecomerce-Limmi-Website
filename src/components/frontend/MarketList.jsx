2b4b6e279ea146c7fbee7c24d4223df641e36c66
azzurritech-danish <danish@azzurri.pk>
2025-11-15 03:03:19 -0800

    change  code style only

--- src/components/frontend/MarketList.jsx
+++ src/components/frontend/MarketList.jsx
@@ -1,21 +1,22 @@
-export const dynamic = 'force-dynamic'
-import React from 'react'
-import MarketCarousole from "@/components/frontend/MarketCarousole"
-import { getData } from '@/lib/getData'
+export const dynamic = "force-dynamic";
+import React from "react";
+import MarketCarousole from "@/components/frontend/MarketCarousole";
+import { getData } from "@/lib/getData";
 
 async function MarketList() {
-  const markets = await getData('markets')
+  const markets = await getData("markets");
 
   return (
-    <div className='py-8'>
-
+    <div className="py-8">
       {/*Market Page Carousel*/}
       <div className="bg-white dark:bg-gray-800 text-center rounded-lg p-6 border border-gray-200 dark:border-gray-700 shadow-lg dark:shadow-md">
-        <h2 className='text-gray-900 dark:text-gray-100 text-2xl font-bold mb-6'>Shop By Market</h2>
+        <h2 className="text-gray-900 dark:text-gray-100 text-2xl font-bold mb-6">
+          Shop By Market
+        </h2>
         <MarketCarousole markets={markets} />
       </div>
     </div>
-  )
+  );
 }
 
-export default MarketList
+export default MarketList;
